a30800e95a9d0842102d5741517753d3b60dcc74
Jacob Knecht <jacobknecht3@gmail.com>
2019-09-26 16:04:50 -0500

    implemented error handling for 'add gig' form

--- routes/gigs.js
+++ routes/gigs.js
@@ -15,4 +15,47 @@ router.get('/', (req, res) => {
 
 router.get('/add', (req, res) => res.render('add'));
 
+router.post('/add', (req, res) => {
+    let { title, technologies, description, budget, contact_email } = req.body;
+    let errors = [];
+    if(!title) {
+        errors.push({ text: "Gigs require a title"});
+    }
+    if(!technologies) {
+        errors.push({ text: "Gigs require a list of technologies"});
+    }
+    if(!description) {
+        errors.push({ text: "Gigs require a description"});
+    }
+    if(!contact_email) {
+        errors.push({ text: "Gigs require a contact email"});
+    }
+    if(errors.length > 0) {
+        res.render('add', {
+            errors,
+            title,
+            technologies,
+            description,
+            budget,
+            contact_email
+        });
+    } else {
+        if(!budget) {
+            budget = "Unknown";
+        } else {
+            budget = `$${budget}`;
+        }
+        technologies = technologies.toLowerCase().replace(/, /g, ',');
+        Gig.create({
+            title,
+            technologies,
+            description,
+            budget,
+            contact_email
+        })
+          .then(gig => res.redirect('/gigs'))
+          .catch(err => console.log(`Error: ${err}`));
+    }
+});
+
 module.exports = router;
