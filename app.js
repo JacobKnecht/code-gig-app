fa59f47804748cf9c0f9a4549559ba9db769c86f
Jacob Knecht <jacobknecht3@gmail.com>
2019-09-26 13:10:24 -0500

    set the view engine to handlebars

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ sequelize.authenticate()
   .catch(err => console.log(`Error: ${err}`));
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+app.engine('handlebars', exphbs({ defaultLayout : 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => res.send('INDEX'));
 
 app.use('/gigs', require('./routes/gigs'));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server listening on port ${PORT}`));
