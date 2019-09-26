8110e149542cf4323c8e9555dadc97298ce68a09
Jacob Knecht <jacobknecht3@gmail.com>
2019-09-26 12:43:27 -0500

    added route for '/gigs' endpoint

--- app.js
+++ app.js
@@ -13,4 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('INDEX'));
 
+app.use('/gigs', require('./routes/gigs'));
+
 app.listen(PORT, console.log(`Server listening on port ${PORT}`));
