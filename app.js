11d288a43c43013b1aebf2af72c261bb8068750e
Jacob Knecht <jacobknecht3@gmail.com>
2019-09-26 12:29:02 -0500

    moved Sequelize initialization code to separate database file

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
+const sequelize = require('./config/database');
+
+sequelize.authenticate()
+  .then(() => console.log('Database connection successful'))
+  .catch(err => console.log(`Error: ${err}`));
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env || 5000;
+app.get('/', (req, res) => res.send('INDEX'));
 
 app.listen(PORT, console.log(`Server listening on port ${PORT}`));
